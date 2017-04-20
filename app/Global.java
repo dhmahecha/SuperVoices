54a0583d85d0c6a9b540712fcfc89b6e2a319667
dianaespc <dpaolac@gmail.com>
2017-04-19 21:45:49 -0500

    Commit

--- app/Global.java
+++ app/Global.java
@@ -1,58 +1,48 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
 import akka.actor.Cancellable;
-import play.*;
+import constantes.IConstantesSuperVoices;
+import play.Application;
+import play.Configuration;
+import play.GlobalSettings;
+import play.Logger;
 import play.libs.Akka;
 import scala.concurrent.duration.Duration;
+import utils.ProcesarAudio;
 
 
 
 public class Global extends GlobalSettings {
 
 	private Cancellable scheduler;
-	
-	public void onStart(Application application) {
-	    int timeDelayFromAppStartToLogFirstLogInMs = 0;
-	    int timeGapBetweenMemoryLogsInMinutes = 30;
-	    scheduler = Akka.system().scheduler().schedule(Duration.create(timeDelayFromAppStartToLogFirstLogInMs, TimeUnit.MILLISECONDS),
-	            Duration.create(timeGapBetweenMemoryLogsInMinutes, TimeUnit.SECONDS),
-	            new Runnable() {
-	                @Override
-	                public void run() {
-	                	try {
-	                        Runtime rt = Runtime.getRuntime();
-	                        Process proc = rt.exec("ffmpeg");
-	                        InputStream stderr = proc.getErrorStream();
-	                        InputStreamReader isr = new InputStreamReader(stderr);
-	                        BufferedReader br = new BufferedReader(isr);
-	                        String line = null;
-	                        System.out.println("<ERROR>");
-	                        while ( (line = br.readLine()) != null)
-	                            System.out.println(line);
-	                        System.out.println("</ERROR>");
-	                        int exitVal = proc.waitFor();
-	                        System.out.println("Process exitValue: " + exitVal);
-						} catch (IOException | InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-	                	System.out.println("Corriendo tarea programada");
-	                }
-	            },
-	            Akka.system().dispatcher());
-	    super.onStart(application);
-	}
-  	    
- 
 
-    public void onStop(Application app) {
-        scheduler.cancel();
-        super.onStop(app);
-    }
-	
+	public void onStart(Application application) {
+		if(Configuration.root().getString(IConstantesSuperVoices.ES_WORKER).equals("true")){
+			int timeDelayFromAppStartToLogFirstLogInMs = 0;
+			int timeGapBetweenMemoryLogsInMinutes = 30;
+			scheduler = Akka.system().scheduler().schedule(Duration.create(timeDelayFromAppStartToLogFirstLogInMs, TimeUnit.MILLISECONDS),
+					Duration.create(timeGapBetweenMemoryLogsInMinutes, TimeUnit.SECONDS),
+					new Runnable() {
+				@Override
+				public void run() {
+					Logger.warn("Tarea programada en ejecución");
+					ProcesarAudio procesar = new ProcesarAudio();
+					procesar.iniciarProceso();
+				}
+			},
+					Akka.system().dispatcher());
+			super.onStart(application);
+		}
+	}
+
+
+
+	public void onStop(Application app) {
+		if(Configuration.root().getString(IConstantesSuperVoices.ES_WORKER).equals("true")){
+			scheduler.cancel();
+			super.onStop(app);
+		}
+	}
+
 }
